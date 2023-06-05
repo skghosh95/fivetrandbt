b2dfcb1c077075fb53bcf5f368261f797d5bb0d2
Suman Kalyan Ghosh <134367203+skghosh95@users.noreply.github.com>
2023-06-05 19:25:16 +0530

    Update Raw_employee.sql

--- models/Raw_employee.sql
+++ models/Raw_employee.sql
@@ -1,11 +1,8 @@
-
-with final as (
-
-
-SELECT
-    Raw_employee.value:_name::VARCHAR as name,
-
-    Raw_employee.value:_id::VARCHAR as id,
-    from {{ source('name', 'id') }}
- )  
- select * from final
+select * from Raw_employee
+(
+    select
+        name,
+          id  
+     
+from {{ ref('Raw_employee') }}
+)
