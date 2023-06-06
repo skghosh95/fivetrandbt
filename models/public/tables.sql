ab75bd378c039dee92bdea43cdf8f7d82dfa9002
Suman Kalyan Ghosh <134367203+skghosh95@users.noreply.github.com>
2023-06-06 11:57:45 +0530

    Update tables.sql

--- models/public/tables.sql
+++ models/public/tables.sql
@@ -2,7 +2,7 @@ with final as (
 select
  raw_employee.value:_name:VARCHAR(60) as name,
  raw_employee.value:_id:NUMBER as id
- from {{ source('csv','suman000') }},
+ from {{ source('csv','Raw_employee') }},
  LATERAL FLATTEN(INPUT => raw_employee) rawemployee_flattened,
  LATERAL FLATTEN(INPUT => rawemployee_flattened.value:raw_employee)
  )
